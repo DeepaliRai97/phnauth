9a20c9f7b444ef3bbf6c8a41ab737391f60679aa
unknown <deepali.rai@kelltontech.com>
2020-10-26 18:46:54 +0530

    my first commmit

---
+++ PhnAuth.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import { Button, TextInput } from 'react-native';
+import auth from '@react-native-firebase/auth';
+
+function App() {
+  // If null, no SMS has been sent
+  const [confirm, setConfirm] = useState(null);
+
+  const [code, setCode] = useState('');
+
+  // Handle the button press
+  async function signInWithPhoneNumber(phoneNumber) {
+    const confirmation = await auth().signInWithPhoneNumber(phoneNumber);
+    setConfirm(confirmation);
+  }
+
+  async function confirmCode() {
+    try {
+      await confirm.confirm(code); 
+      console.log(code)
+    } catch (error) {
+      console.log(error)
+      console.log('Invalid code.');
+    }
+  }
+
+  if (!confirm) {
+    return (
+      <Button
+        title="Phone Number Sign In"
+        onPress={() => signInWithPhoneNumber('+91 8826144145')}
+      />
+    ); 
+  }
+
+  return (
+    <>
+      <TextInput value={code} onChangeText={text => setCode(text)} />
+      <Button title="Confirm Code" onPress={() => confirmCode()} />
+    </>
+  );
+}
+
+export default App;
